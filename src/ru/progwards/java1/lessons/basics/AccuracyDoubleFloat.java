f617c507189d0a6d52fbf99bde7d4269cd0b74b6
Masha <mashas-v@bk.ru>
2020-06-28 22:01:39 +0400

    lesson3

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -1,29 +1,33 @@
 package ru.progwards.java1.lessons.basics;
 
 public class AccuracyDoubleFloat {
-    final static double Pi = 3.14;
 
     public static double volumeBallDouble(double radius) { //возвращает объём шара с радиусом radius и основана на типе double.
+        radius = 6371.2;
+        final double Pi = 3.14;
         final double R = radius * radius * radius;
-        return 4/(3 * Pi * R);
+        return 4 * Pi * R / 3;
     }
     public static float volumeBallFloat(float radius) { //возвращает объём шара с радиусом radius и основана на типе float
+        radius = 6371.2F;
+        float b = 3.14F;
         final float R = radius * radius * radius;
-        return (float) (4/(3 * Pi * R));
+        return (float) (4 * b * R / 3);
     }
     public static double calculateAccuracy(double radius) { //возвращает разницу между функциями volumeBallDouble и volumeBallFloat (они должны быть вызваны из неё с параметром radius)
         radius = 6371.2;
+        final double Pi = 3.14;
         final double R = radius * radius * radius;
         float b = (float) 3.14F;
         float temp = (float) 6371.2F;
         final float T = temp * temp * temp;
-        return 4/(3 * Pi * R) - (float) 4/(3 * b * T);
+        return 4 * Pi * R / 3 - (float) (4 * b * T / 3);
     }
 
     public static void main(String[] args) {
         System.out.println(volumeBallDouble(1));
         System.out.println(volumeBallDouble(6371.2));
         System.out.println(volumeBallFloat(6371.2F));
-        System.out.println(calculateAccuracy(6371.2));
+        System.out.println(calculateAccuracy(volumeBallDouble(6371.2)-volumeBallFloat(6371.2F)));
     }
 }
