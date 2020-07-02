a9717e3e013b0f6ad792321ba7757f42586acb88
Masha <mashas-v@bk.ru>
2020-07-02 10:27:58 +0400

    test 4_2

--- src/my/all/test/TTest4_2.java
+++ src/my/all/test/TTest4_2.java
@@ -1,4 +1,13 @@
 package my.all.test;
 
 public class TTest4_2 {
+    public static void main(String[] args) {
+        if (1 > 2 && 5 >= 0)
+
+            System.out.println("Что-то из этого условия точно верно");
+
+        else
+
+            System.out.println("Быть может что-то в этом условии верно, но что-то точно нет");
+    }
 }
