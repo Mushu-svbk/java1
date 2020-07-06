c32fd30a76749ad0ce38aa2059779448b0b5d92f
Masha <mashas-v@bk.ru>
2020-07-07 03:59:57 +0400

    test 5_1

---
+++ src/my/all/test/Person.java
@@ -0,0 +1,37 @@
+package my.all.test;
+
+public class Person {
+    private String name;
+    private int age;
+    private String country;
+
+    public Person() {
+        country = "RU";
+    }
+
+    public Person(String name, int age) {
+        this();
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public static void main(String[] args) {
+        String nameValue = "Мария";
+        Person Samuylova = new Person(nameValue, 37);
+        System.out.println(Samuylova.age);
+        System.out.println(Samuylova.country);
+        System.out.println(Samuylova.name);
+     }
+}
