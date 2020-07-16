ef9ea2011feec3d3e818e7f6687e1d4cc24a1827
Masha <mashas-v@bk.ru>
2020-07-16 04:30:38 +0400

    v1.3

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -3,33 +3,33 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 public class TriangleSimpleInfo {
 
     public static int maxSide(int a, int b, int c) { //сверяет длины сторон треугольника, и возвращает наибольшую длину стороны.
-        if ((a >= b & a >= c))
+        if ((a >= b && a >= c))
             return a;
-        else if ((b >= a & b >= c))
+        else if ((b >= a && b >= c))
             return b;
-        else if ((c >= a & c >= b))
+        else if ((c >= a && c >= b))
             return c;
-        else if (a == b & a == c)
+        else if (a == b && a == c)
             return a;
         else
             return 0;
     }
 
     public static int minSide(int a, int b, int c) { //сверяет длины сторон треугольника, и возвращает наименьшую длину стороны.
-        if ((a <= b & a <= c))
+        if ((a <= b && a <= c))
             return a;
-        else if ((b <= a & b <= c))
+        else if ((b <= a && b <= c))
             return b;
-        else if ((c <= a & c <= b))
+        else if ((c <= a && c <= b))
             return c;
-        else if (a == b & a == c)
+        else if (a == b && a == c)
             return a;
         else
             return 0;
     }
 
     public static boolean isEquilateralTriangle(int a, int b, int c) { // возвращает true, если треугольник равносторонний и false в противном случае.
-        if (a == b & a == c)
+        if (a == b && a == c)
             return true;
         else
             return false;
