c9c7622e33a74e804d20b47734d273b8bddff5fe
Masha <mashas-v@bk.ru>
2020-07-27 05:59:19 +0400

    v1.8

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -20,22 +20,27 @@ public class Eratosthenes { //массив чисел, собственно, "р
 
 
     private void sift() { //метод который, собственно и реализует алгоритм Эратосфена, просеивая составные числа. Подсказка - нужно реализовать 2 вложенных цикла, внешний, например по i от 2 до N-1, и внутренний, например по j который будет просеивать числа, кратные переменной внешнего цикла i*j.
-        for (int i = 0; i < this.n; i++) sieve[i] = true;
+        for (int i = 0; i < this.n; i++) {
+            sieve[i] = true;
+        }
 
-        for (int J = 2; J * J <= this.n; J++)
-
-            if (sieve[J] == true)
-
-                for (int i = J * J; i <= this.n; i += J)
-
+        for (int j= 2; j * j <= this.n; j++) {
+            if (sieve[j]) {
+                for (int i = j * j; i <= this.n; i += j) {
                     sieve[i] = false;
+                }
+            }
+        }
     }
 
     public boolean isSimple(int n) { //метод, который возвращает sieve[n], что бы можно было узнать, простое число n или составное
         final boolean s;
-        if (sieve[n]) s = true;
-        else s = false;
-        return s;
+        if (n % 2 == 0 && n != 2) return false;
+        int maxDiv = (int) Math.sqrt(n);
+        for (int i = 3; i <= maxDiv; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
     }
 
     @Override
@@ -47,10 +52,9 @@ public class Eratosthenes { //массив чисел, собственно, "р
         Eratosthenes era = new Eratosthenes(25);
         System.out.println("N = " + era + ", sieve = " + Arrays.toString(era.sieve));
         System.out.println(era.n);
-        System.out.println("2" + era.isSimple(2));
-        System.out.println("4" + era.isSimple(4));
-        System.out.println("30" + era.isSimple(30));
-        System.out.println("9" + era.isSimple(9));
-        System.out.println("3" + era.isSimple(3));
+        System.out.println("2 " + era.isSimple(2));
+        System.out.println("4 " + era.isSimple(4));
+        System.out.println("9 " + era.isSimple(9));
+        System.out.println("3 " + era.isSimple(3));
     }
 }
