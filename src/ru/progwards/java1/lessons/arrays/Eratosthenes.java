b2adf7b78a0cb59bbd9fdd2dbbdb9311d80b7163
Masha <mashas-v@bk.ru>
2020-07-31 22:19:33 +0400

    v1.23

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -4,10 +4,6 @@ import java.util.Arrays;
 
 public class Eratosthenes { //массив чисел, собственно, "решето"
     private final boolean[] sieve;
-    int n;
-    int i;
-    int N;
-
 
     public Eratosthenes(int N) { //конструктор, который должен разместить массив sieve с размером в N и заполнить его значениями true, после чего вызвать метод sift()
         sieve = new boolean[N];
@@ -17,10 +13,11 @@ public class Eratosthenes { //массив чисел, собственно, "р
     }
 
     private void sift() { //метод который, собственно и реализует алгоритм Эратосфена, просеивая составные числа. Подсказка - нужно реализовать 2 вложенных цикла, внешний, например по i от 2 до N-1, и внутренний, например по j который будет просеивать числа, кратные переменной внешнего цикла i*j.
-        for (int j = 2; j * j <= N; j++) {
-            if (sieve[i]) {
-                for (int i = j * j; i <= N; i+= j) {
+        for (int j = 2; j * j < sieve.length; j++) {
+             {
+                for (int i = j * j; i < sieve.length; i+= j) {
                     sieve[i] = false;
+                    System.out.println(i);
                 }
             }
         }
@@ -33,15 +30,11 @@ public class Eratosthenes { //массив чисел, собственно, "р
     public static void main(String[] args) {
         Eratosthenes era = new Eratosthenes(15);
         System.out.println("N = " + era + ", sieve = " + Arrays.toString(era.sieve));
-        System.out.println(era.n);
+//        System.out.println(era.n);
         System.out.println("2 " + era.isSimple(2));
         System.out.println("4 " + era.isSimple(4));
         System.out.println("9 " + era.isSimple(9));
         System.out.println("3 " + era.isSimple(3));
         System.out.println("7 " + era.isSimple(7));
-    }
-
-    public String toString() {
-        return "Решето Эратосфена{" + n + "}";
     }
 }
